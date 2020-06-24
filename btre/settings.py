28198bb4296a88251ea25e31dbb1911517345292
Chaitanya Pandit <52273408+ChaitanyaPandit1998@users.noreply.github.com>
2020-06-24 14:37:39 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -147,6 +147,6 @@ MESSAGE_TAGS={
 #Email Config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'apptravelling01@gmail.com'
-EMAIL_HOST_PASSWORD = 'travelapp@1'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS=True
